8ad0a7cca13becfd30da5200defe016a8af84c3f
fredalden <raymondcasy@gmail.com>
2023-09-17 19:15:25 +0000

    Ajout d'un nouveau user et message

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ config = dotenv_values(".env")
 BOT_TOKEN = config['KEY']
 CELUI_A_FAIRE_CHIER = config['CELUI_A_FAIRE_CHIER']
 MESSAGE_QUI_FAIT_CHIER = config['MESSAGE_QUI_FAIT_CHIER']
+LE_SANG= config['LE_SANG']
+MESSAGE_AU_SANG=config['MESSAGE_AU_SANG']
+
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -26,5 +29,8 @@ async def on_message(message):
     if message.author.id == CELUI_A_FAIRE_CHIER:
         await message.author.send(f'{MESSAGE_QUI_FAIT_CHIER}')
         logger.info(f"Sending {MESSAGE_QUI_FAIT_CHIER} to {message.author}")
+    if message.author.id == LE_SANG:
+        await message.author.send(f'{MESSAGE_AU_SANG}')
+        logger.info(f"Sending {MESSAGE_AU_SANG} to {message.author}")
 
 client.run(f'{BOT_TOKEN}')
